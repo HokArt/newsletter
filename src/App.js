84b3e200557d2fbf33c8bcf5384a546e05a8b154
HokArt <kokouhoka@gmail.com>
2023-11-25 10:48:57 +0000

    commit3

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
 import React from "react";
 import Main from "./main/main";
 import Success from "./components/success";
-import { BrowserRouter, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './styles/app.css';
 
 const router = createBrowserRouter([
   {
-    path: '/newsletter',
+    path: '/',
     element: <Main/>
   },
   {
-    path: '/newsletter/success',
+    path: '/success',
     element: <Success/>
   }
 ])
 
 function App() {
   return (
-    <BrowserRouter basename="/newsletter">
-      <div className="App">
-        <RouterProvider router={router}/>
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <RouterProvider router={router}/>
+    </div>
   );
 }
 
